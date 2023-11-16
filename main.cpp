8c62534cb44757b0eb55a9acf9790279bb5e048d
Achille Nazaret <achille.nazaret@gmail.com>
2023-11-15 22:44:53 -0500

    example main

--- main.cpp
+++ main.cpp
@@ -1,22 +1,26 @@
+#include "BICScorer.h"
+#include "PDAG.h"
+#include "XGES.h"
 #include <iostream>
-#include "PDAG.h" // Include the header file for the PDAG class
+
+#include "cnpy.h"
+typedef Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> RowMajorMatrixXd;
 
 int main() {
-    // Create a PDAG instance with nodes 0, 1, 2, 3, and 4
-    PDAG pdag(5);
+    // Seed random number generator of Eigen
+    srand(0);
+    // read MatrixXd from file
+    //save it to file
+    cnpy::NpyArray arr = cnpy::npy_load("arr2.npy");
+    RowMajorMatrixXd m = Eigen::Map<RowMajorMatrixXd>(arr.data<double>(), arr.shape[0], arr.shape[1]);
 
-    pdag.add_directed_edge(0, 1);
-    pdag.add_undirected_edge(2, 3);
+    BICScorer scorer(m, 1);
+    //    std::cout << scorer.local_diff_score(4, {0}, 3) << std::endl;
 
-    std::cout << "Number of edges: " << pdag.get_number_of_edges() << std::endl;
-    // show the list of directed edges
-    std::cout << "Directed edges: ";
-    for (const auto &edge: pdag.get_directed_edges()) {
-        std::cout << "(" << edge.first << ", " << edge.second << ") ";
-    }
+    XGES xges(m, &scorer);
 
-    std::cout << std::endl << "Is clique (1,2,3): ";
-    std::cout << pdag.is_clique({1, 2, 3}) << std::endl;
+    xges.fit_heuristic();
+
 
     return 0;
 }
