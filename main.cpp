26e7cb3afa19ae7923fa8f1d39d4563596bb717f
Achille Nazaret <achille.nazaret@gmail.com>
2023-11-17 23:21:37 -0500

    update main

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main() {
     srand(0);
     // read MatrixXd from file
     //save it to file
-    cnpy::NpyArray arr = cnpy::npy_load("arr2.npy");
+    cnpy::NpyArray arr = cnpy::npy_load("arr4.npy");
     RowMajorMatrixXd m = Eigen::Map<RowMajorMatrixXd>(arr.data<double>(), arr.shape[0], arr.shape[1]);
 
     BICScorer scorer(m, 1);
@@ -19,8 +19,18 @@ int main() {
 
     XGES xges(m, &scorer);
 
+    clock_t start = clock();
     xges.fit_heuristic();
+    clock_t end = clock();
+    double elapsed_secs = double(end - start) / CLOCKS_PER_SEC;
+    std::cout << "Time elapsed: " << elapsed_secs << std::endl;
+    std::cout << "Score: " << xges.get_score() << std::endl;
+    std::cout << std::endl;
 
+    // print all statistics in xges.get_pdag().statistics
+    std::cout << "Statistics: " << std::endl;
+    for (auto &kv: xges.get_pdag().statistics) { std::cout << kv.first << ": " << kv.second << std::endl; }
+    for (auto &kv: xges.statistics) { std::cout << kv.first << ": " << kv.second << std::endl; }
 
     return 0;
 }
